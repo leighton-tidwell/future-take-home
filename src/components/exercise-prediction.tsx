f66986a2707a645d93a91f98c49c8b6c44fb2d01
Leighton Tidwell <leighton@tidwells.world>
2025-01-22 22:52:40 -0600

    make difficulty level more intuitive

--- src/components/exercise-prediction.tsx
+++ src/components/exercise-prediction.tsx
@@ -5,21 +5,45 @@ export function ExercisePrediction({
 }: {
   prediction: ExercisePrediction;
 }) {
+  const difficultyLevels = {
+    1: {
+      label: "Beginner",
+      color: "bg-green-500",
+    },
+    2: {
+      label: "Intermediate",
+      color: "bg-yellow-500",
+    },
+    3: {
+      label: "Advanced",
+      color: "bg-red-500",
+    },
+  };
+
   return (
     <div className="flex flex-row gap-2">
-      <div className="text-md">Skill Level:</div>
       <div className="text-md w-1/3 bg-gray-100 rounded-md p-2 flex flex-row gap-2">
         {Array.from({ length: prediction.skill_level.max_level }).map(
-          (_, index) => (
-            <div
-              key={index}
-              className={`h-2 w-1/3 ${
-                index < prediction.skill_level.level
-                  ? "bg-blue-500"
-                  : "bg-gray-500"
-              }`}
-            />
-          )
+          (_, index) => {
+            const level = (index + 1) as keyof typeof difficultyLevels;
+            const isActive = level === prediction.skill_level.level;
+
+            return (
+              <div
+                key={index}
+                className={`h-fit w-1/3 items-center justify-center flex p-2 rounded-md ${
+                  isActive
+                    ? difficultyLevels[
+                        prediction.skill_level
+                          .level as keyof typeof difficultyLevels
+                      ].color
+                    : "bg-gray-500"
+                }`}
+              >
+                {difficultyLevels[level].label}
+              </div>
+            );
+          }
         )}
       </div>
     </div>
